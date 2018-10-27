ec57a9220772b12ac04e9023bc47f14f4c3575a6
OAC <overallcoma@gmail.com>
2018-10-26 23:42:58 -0500

    Initial Commit

--- dockerfile
+++ dockerfile
@@ -5,6 +5,8 @@ RUN mkdir /app
 COPY c2_community-linux-64 /app
 COPY run.sh /app
 RUN chmod +x /app/c2_community-linux-64
-EXPOSE 80 443 8080 2022
+RUN apt update -y
+RUN apt install -y net-tools
+EXPOSE 8080 2022
 
 CMD /bin/bash /app/run.sh
--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-IPv4ADDRESS = ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'
+IPv4ADDRESS=$"(ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')"
 /app/c2_community-linux-64 -hostname $IPv4Address
